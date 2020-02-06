fbe740e012368bf406c5f081d8894d458083e7d7
ruleGreen <2533573828@qq.com>
2020-02-06 21:19:37 +0800

    add demo

--- errCorrect/js/js.cookie.js
+++ errCorrect/js/js.cookie.js
@@ -1,68 +1,88 @@
-/*! js-cookie v3.0.0-beta.3 | MIT */
-;
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, (function () {
-    var current = global.Cookies;
-    var exports = global.Cookies = factory();
-    exports.noConflict = function () { global.Cookies = current; return exports; };
-  }()));
-}(this, function () { 'use strict';
-
-  var rfc6265Converter = {
-    read: function (value) {
-      return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent)
-    },
-    write: function (value) {
-      return encodeURIComponent(value).replace(
-        /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
-        decodeURIComponent
-      )
-    }
-  };
-
-  function assign (target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
-      for (var key in source) {
-        target[key] = source[key];
+/*!
+ * JavaScript Cookie v2.2.1
+ * https://github.com/js-cookie/js-cookie
+ *
+ * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
+ * Released under the MIT license
+ */
+;(function (factory) {
+  var registeredInModuleLoader;
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+    registeredInModuleLoader = true;
+  }
+  if (typeof exports === 'object') {
+    module.exports = factory();
+    registeredInModuleLoader = true;
+  }
+  if (!registeredInModuleLoader) {
+    var OldCookies = window.Cookies;
+    var api = window.Cookies = factory();
+    api.noConflict = function () {
+      window.Cookies = OldCookies;
+      return api;
+    };
+  }
+}(function () {
+  function extend () {
+    var i = 0;
+    var result = {};
+    for (; i < arguments.length; i++) {
+      var attributes = arguments[ i ];
+      for (var key in attributes) {
+        result[key] = attributes[key];
       }
     }
-    return target
+    return result;
   }
 
-  function init (converter, defaultAttributes) {
+  function decode (s) {
+    return s.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent);
+  }
+
+  function init (converter) {
+    function api() {}
+
     function set (key, value, attributes) {
       if (typeof document === 'undefined') {
-        return
+        return;
       }
 
-      attributes = assign({}, defaultAttributes, attributes);
+      attributes = extend({
+        path: '/'
+      }, api.defaults, attributes);
 
       if (typeof attributes.expires === 'number') {
-        attributes.expires = new Date(Date.now() + attributes.expires * 864e5);
-      }
-      if (attributes.expires) {
-        attributes.expires = attributes.expires.toUTCString();
+        attributes.expires = new Date(new Date() * 1 + attributes.expires * 864e+5);
       }
 
-      value = converter.write(value, key);
+      // We're using "expires" because "max-age" is not supported by IE
+      attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
 
-      key = encodeURIComponent(key)
-        .replace(/%(2[346B]|5E|60|7C)/g, decodeURIComponent)
-        .replace(/[()]/g, escape);
+      try {
+        var result = JSON.stringify(value);
+        if (/^[\{\[]/.test(result)) {
+          value = result;
+        }
+      } catch (e) {}
+
+      value = converter.write ?
+        converter.write(value, key) :
+        encodeURIComponent(String(value))
+          .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+
+      key = encodeURIComponent(String(key))
+        .replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
+        .replace(/[\(\)]/g, escape);
 
       var stringifiedAttributes = '';
       for (var attributeName in attributes) {
         if (!attributes[attributeName]) {
-          continue
+          continue;
         }
-
         stringifiedAttributes += '; ' + attributeName;
-
         if (attributes[attributeName] === true) {
-          continue
+          continue;
         }
 
         // Considers RFC 6265 section 5.2:
@@ -75,68 +95,69 @@
         stringifiedAttributes += '=' + attributes[attributeName].split(';')[0];
       }
 
-      return (document.cookie = key + '=' + value + stringifiedAttributes)
+      return (document.cookie = key + '=' + value + stringifiedAttributes);
     }
 
-    function get (key) {
-      if (typeof document === 'undefined' || (arguments.length && !key)) {
-        return
+    function get (key, json) {
+      if (typeof document === 'undefined') {
+        return;
       }
 
+      var jar = {};
       // To prevent the for loop in the first place assign an empty array
       // in case there are no cookies at all.
       var cookies = document.cookie ? document.cookie.split('; ') : [];
-      var jar = {};
-      for (var i = 0; i < cookies.length; i++) {
+      var i = 0;
+
+      for (; i < cookies.length; i++) {
         var parts = cookies[i].split('=');
         var cookie = parts.slice(1).join('=');
 
-        if (cookie[0] === '"') {
+        if (!json && cookie.charAt(0) === '"') {
           cookie = cookie.slice(1, -1);
         }
 
         try {
-          var name = rfc6265Converter.read(parts[0]);
-          jar[name] = converter.read(cookie, name);
+          var name = decode(parts[0]);
+          cookie = (converter.read || converter)(cookie, name) ||
+            decode(cookie);
+
+          if (json) {
+            try {
+              cookie = JSON.parse(cookie);
+            } catch (e) {}
+          }
+
+          jar[name] = cookie;
 
           if (key === name) {
-            break
+            break;
           }
         } catch (e) {}
       }
 
-      return key ? jar[key] : jar
+      return key ? jar[key] : jar;
     }
 
-    return Object.create(
-      {
-        set: set,
-        get: get,
-        remove: function (key, attributes) {
-          set(
-            key,
-            '',
-            assign({}, attributes, {
-              expires: -1
-            })
-          );
-        },
-        withAttributes: function (attributes) {
-          return init(this.converter, assign({}, this.attributes, attributes))
-        },
-        withConverter: function (converter) {
-          return init(assign({}, this.converter, converter), this.attributes)
-        }
-      },
-      {
-        attributes: { value: Object.freeze(defaultAttributes) },
-        converter: { value: Object.freeze(converter) }
-      }
-    )
+    api.set = set;
+    api.get = function (key) {
+      return get(key, false /* read as raw */);
+    };
+    api.getJSON = function (key) {
+      return get(key, true /* read as json */);
+    };
+    api.remove = function (key, attributes) {
+      set(key, '', extend(attributes, {
+        expires: -1
+      }));
+    };
+
+    api.defaults = {};
+
+    api.withConverter = init;
+
+    return api;
   }
 
-  var api = init(rfc6265Converter, { path: '/' });
-
-  return api;
-
+  return init(function () {});
 }));
